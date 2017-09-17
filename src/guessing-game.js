65b0b40ffe208a057b471e9f370dc62b2bcc7772
Polina <31920325+kartelpol@users.noreply.github.com>
2017-09-17 15:58:41 +0200

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+		this.root = null;
+		this.max = null;
+		this.min = null;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
+    	this.root = Math.round((this.max + this.min)/2);
+    	return this.min;
+    	return this.max;
     }
 
     guess() {
-
+    	this.root = Math.round((this.max + this.min)/2);
+    	return this.root;
     }
 
     lower() {
-
+    	this.max = this.root;
+    	return this.max;
     }
 
     greater() {
-
+		this.min = this.root;
+		return this.min;
     }
 }
 
